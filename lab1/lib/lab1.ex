e2d3e7292c4b2d7524042dc061322e3ff2ec22b6
VictorIstratii151 <victor.istratii@faf.utm.md>
2018-02-08 10:52:01 +0200

    Created a dummy Elixir project

---
+++ lab1/lib/lab1.ex
@@ -0,0 +1,18 @@
+defmodule LAB1 do
+  @moduledoc """
+  Documentation for LAB1.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LAB1.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
