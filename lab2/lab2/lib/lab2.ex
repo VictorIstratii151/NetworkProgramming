8a3f4a9f2bdc16cc573d4075bbdba7695d616885
VictorIstratii151 <victor.istratii@faf.utm.md>
2018-03-14 20:15:43 +0200

    Created a project for lab 2

---
+++ lab2/lab2/lib/lab2.ex
@@ -0,0 +1,18 @@
+defmodule LAB2 do
+  @moduledoc """
+  Documentation for LAB2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LAB2.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
