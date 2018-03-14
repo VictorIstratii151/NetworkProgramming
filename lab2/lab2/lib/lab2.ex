a8646ccafd22cc89ee47fa629759783e204979d7
VictorIstratii151 <victor.istratii@faf.utm.md>
2018-03-14 23:28:10 +0200

    Added a function for fetching the data and a function for printing it

--- lab2/lab2/lib/lab2.ex
+++ lab2/lab2/lib/lab2.ex
@@ -25,6 +25,42 @@ defmodule LAB2 do
     end
   end
 
- 
+  defp pretty_print(row) do
+    sensor_type = case row["sensor_type"] do
+        0 -> "Temperature sensor"
+        1 -> "Humidity sensor"
+        2 -> "Motion sensor"
+        3 -> "Alien Presence detector"
+        4 -> "Dark Matter detector"
+        5 -> "Midichlorian analyzer"
+        _ -> "Do we even have it?"
+    end
+    [
+    "[" <> sensor_type <> "]" <> "\n",
+    :white, :bright,
+    "Device ID: "  <> row["device_id"]
+    <> " with value " <> Float.to_string(row["value"])
+    <> "\n"] |> IO.ANSI.format |> IO.puts
+  end
+
+  def fetch do
+      {urls, key} = get_urls_and_key()
+
+      urls
+      |> Enum.map(fn url ->
+              Task.async(fn ->
+                  HTTPotion.get "#{@service_root}#{url["path"]}",
+                                [headers: ["session": key],
+                                 timeout: 50_000]
+              end)
+          end)
+      |> Enum.map(&Task.await(&1, 50_000))
+      |> Enum.map(&get_body_and_conttype(&1))
+      |> Enum.map(&parse(&1))
+      |> List.flatten
+      |> Enum.filter(fn x -> x != :ok end)
+      |> Enum.sort(fn x, y -> x["sensor_type"] < y["sensor_type"] end)
+      |> Enum.each(&pretty_print(&1))
+  end
 
 end
