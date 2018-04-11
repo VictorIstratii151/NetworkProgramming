cf495cb82cc6e5ab1704dacc14820caf03acaa8c
Victor Istratii <victor.istratii@gmail.com>
2018-04-11 11:12:54 +0300

    Created a project for the first task in lab 2-3

---
+++ lab2/lab21/lib/lab21.ex
@@ -0,0 +1,18 @@
+defmodule Lab21 do
+  @moduledoc """
+  Documentation for Lab21.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Lab21.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lab2/lab21/test/lab21_test.exs
@@ -0,0 +1,8 @@
+defmodule Lab21Test do
+  use ExUnit.Case
+  doctest Lab21
+
+  test "greets the world" do
+    assert Lab21.hello() == :world
+  end
+end
