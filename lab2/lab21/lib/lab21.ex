17f4974a0c639d09c16448012fdf5f12c5820413
Victor Istratii <victor.istratii@gmail.com>
2018-04-12 23:13:18 +0300

    Added a function to separate the root categories from child categories

--- lab2/lab21/lib/lab21.ex
+++ lab2/lab21/lib/lab21.ex
@@ -3,37 +3,68 @@ defmodule Lab21 do
   @categories_url Application.fetch_env!(:lab21, :categories_url)
   @secret_key Application.fetch_env!(:lab21, :secret_key)
 
-  def fetch_data(:categories) do
-    case HTTPoison.request(
-           :get,
-           @categories_url,
-           "",
-           [{"Accept", "text/csv"}, {"X-API-Key", @secret_key}],
-           timeout: 30000,
-           recv_timeout: 30000
-         ) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        IO.puts(body)
+  @test_start "2018-04-10"
+  @test_end "2018-04-12"
 
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect(reason)
-    end
+  def fetch_data(:categories) do
+    categories =
+      case HTTPoison.request(
+             :get,
+             @categories_url,
+             "",
+             [{"Accept", "text/csv"}, {"X-API-Key", @secret_key}],
+             timeout: 30000,
+             recv_timeout: 30000
+           ) do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+          body
+
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          IO.inspect(reason)
+      end
+
+    categories
   end
 
   def fetch_data(:orders, start_date, end_date) do
-    case HTTPoison.request(
-           :get,
-           @orders_url <> "?start=" <> start_date <> "&end=" <> end_date,
-           "",
-           [{"Accept", "text/csv"}, {"X-API-Key", @secret_key}],
-           timeout: 30000,
-           recv_timeout: 30000
-         ) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        IO.puts(body)
+    orders =
+      case HTTPoison.request(
+             :get,
+             @orders_url <> "?start=" <> start_date <> "&end=" <> end_date,
+             "",
+             [{"Accept", "text/csv"}, {"X-API-Key", @secret_key}],
+             timeout: 30000,
+             recv_timeout: 30000
+           ) do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+          # IO.puts(body)
+          body
 
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect(reason)
-    end
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          IO.inspect(reason)
+      end
+
+    orders
+  end
+
+  def ords do
+    fetch_data(:orders, @test_start, @test_end)
+  end
+
+  def separate_categories do
+    categories = fetch_data(:categories) |> Lab21.Parsers.decode_csv()
+
+    Enum.split_with(categories, fn elem ->
+      decision =
+        case elem do
+          {:ok, [_, _, ""]} ->
+            true
+
+          {:ok, _} ->
+            false
+        end
+
+      decision
+    end)
   end
 end
