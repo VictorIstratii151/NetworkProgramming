6270ea1dbf8c941a6bf60fdcf7866d358e21e14f
Victor Istratii <victor.istratii@gmail.com>
2018-04-24 20:12:02 +0300

    Added 2 utility functions for csv handling

--- lab2/lab21/lib/parsers.ex
+++ lab2/lab21/lib/parsers.ex
@@ -1,5 +1,14 @@
 defmodule Lab21.Parsers do
-  def decode_csv(data) do
+  def parse_csv_string(data) do
     data |> String.split("\n") |> CSV.decode() |> Enum.to_list() |> Keyword.take([:ok])
+  end
+
+  def read_csv_file(filename) do
+    File.stream!(filename) |> CSV.decode() |> Enum.to_list() |> Keyword.get_values(:ok)
+  end
+
+  def encode_csv(table_data) do
+    file = File.open!("serialized_totals.csv", [:write, :utf8])
+    table_data |> CSV.encode() |> Enum.each(&IO.write(file, &1))
   end
 end
