73f7891e0586a88910b5f0fd30820282ca9feb7c
Victor Istratii <victor.istratii@faf.utm.md>
2018-05-02 22:38:36 +0300

    Defined the clauses for handling info, cast and disconnect

--- lab5/client/lib/client.ex
+++ lab5/client/lib/client.ex
@@ -17,4 +17,37 @@ defmodule TcpClient do
     send(self(), :connect)
     {:ok, state}
   end
+
+  def handle_info(:connect, state) do
+    Logger.info("Connecting to #{:inet.ntoa(@ip)}:#{@port}")
+
+    case :gen_tcp.connect(@ip, @port, [:binary, active: true]) do
+      {:ok, socket} ->
+        {:noreply, %{state | socket: socket}}
+
+      {:error, reason} ->
+        disconnect(state, reason)
+    end
+  end
+
+  def handle_info({:tcp, _, data}, state) do
+    Logger.info("Received #{data}")
+
+    {:noreply, state}
+  end
+
+  def handle_info({:tcp_closed, _}, state), do: {:stop, :normal, state}
+  def handle_info({:tcp_error, _}, state), do: {:stop, :normal, state}
+
+  def handle_cast({:message, message}, %{socket: socket} = state) do
+    Logger.info("Sending #{message}")
+
+    :ok = :gen_tcp.send(socket, message)
+    {:noreply, state}
+  end
+
+  def disconnect(state, reason) do
+    Logger.info("Disconnected: #{reason}")
+    {:stop, :normal, state}
+  end
 end
