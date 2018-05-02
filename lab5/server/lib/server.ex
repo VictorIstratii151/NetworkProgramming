b9b09084b80856dac50f5f0dc2c3fccabf9a2431
Victor Istratii <victor.istratii@faf.utm.md>
2018-05-02 22:45:01 +0300

    Defined the handle_info clauses

--- lab5/server/lib/server.ex
+++ lab5/server/lib/server.ex
@@ -15,4 +15,23 @@ defmodule TcpServer do
     Logger.info("Accepting connection on port #{@port}...")
     {:ok, %{state | socket: socket}}
   end
+
+  def handle_info(:accept, %{socket: socket} = state) do
+    {:ok, _} = :gen_tcp.accept(socket)
+
+    Logger.info("Client connected")
+    {:noreply, state}
+  end
+
+  def handle_info({:tcp, socket, data}, state) do
+    Logger.info("Received #{data}")
+    Logger.info("Sending it back")
+
+    :ok = :gen_tcp.send(socket, data)
+
+    {:noreply, state}
+  end
+
+  def handle_info({:tcp_closed, _}, state), do: {:stop, :normal, state}
+  def handle_info({:tcp_error, _}, state), do: {:stop, :normal, state}
 end
