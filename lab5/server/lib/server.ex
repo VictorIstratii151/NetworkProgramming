92560b9d1f9675538ba0c86a1e60ac4a0b06b000
Victor Istratii <victor.istratii@faf.utm.md>
2018-05-02 22:44:38 +0300

    Defined the entry function and the init callback

--- lab5/server/lib/server.ex
+++ lab5/server/lib/server.ex
@@ -3,4 +3,16 @@ defmodule TcpServer do
   use GenServer
 
   @port 8000
+
+  def start do
+    GenServer.start(__MODULE__, %{socket: nil})
+  end
+
+  def init(state) do
+    {:ok, socket} = :gen_tcp.listen(@port, [:binary, active: true])
+    send(self(), :accept)
+
+    Logger.info("Accepting connection on port #{@port}...")
+    {:ok, %{state | socket: socket}}
+  end
 end
